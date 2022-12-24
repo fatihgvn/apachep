e7a867b638f37bab13db6b2920235bf165d5356e
FatihGvn <fatihguven@hodein.com>
2022-12-24 20:39:11 +0300

    update

--- install/install-ubuntu.sh
+++ install/install-ubuntu.sh
@@ -146,10 +146,13 @@ clear
 # source /root/.bash_profile
 
 
-echo "export APACHEP='$INSTALL_DIR/system/bin'" > /etc/profile.d/apachep.sh
-echo 'PATH=$PATH:'$INSTALL_DIR'/system/bin' >> /etc/profile.d/apachep.sh
+echo "export APACHEP='$INSTALL_DIR'" > /etc/profile.d/apachep.sh
 chmod 755 /etc/profile.d/apachep.sh
 source /etc/profile.d/apachep.sh
+
+echo 'PATH=$PATH:'$INSTALL_DIR'/system/bin' >> /root/.bash_profile
+echo 'export PATH' >> /root/.bash_profile
+source /root/.bash_profile
 
 # echo '' >> /home/$SUDO_USER/.profile
 # echo '############### APACHEP ###############' >> /home/$SUDO_USER/.profile
