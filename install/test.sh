b190b969bbcb0dc113b46bbc26d08a23d751a0f6
FatihGvn <fatihguven@hodein.com>
2022-12-27 19:00:38 +0300

    remove test sh file in install

--- install/test.sh
+++
@@ -1,7 +0,0 @@
-
-if ! grep -q "IncludeOptional /usr/local/apachep/system/hosts/*.conf" /etc/apache2/apache2.conf; then
-  echo "yeni satır eklendi"
-	sed -i '/IncludeOptional\ mods\-enabled\/\*\.conf/a IncludeOptional /usr/local/apachep/system/hosts/*.conf' /etc/apache2/apache2.conf
-else
-  echo "zaten eklenmiş"
-fi
