1d66ffdc52ab57dac40f971ac94c6a5172856092
Fatih Güven <fatihguven@hodein.com>
2025-02-14 21:38:39 +0300

    updateeee

--- install/ubuntu/install-lxc.sh
+++ install/ubuntu/install-lxc.sh
@@ -115,12 +115,9 @@ lxc-start -n apachep
 
 # 10. Wait for the container to start running.
 echo "Waiting for container's network to be ready..."
-for i in {1..15}; do
-  if lxc-attach -n apachep -- bash -c "grep -q '^nameserver' /etc/resolv.conf"; then
-    echo "Network is ready."
-    break
-  fi
-  sleep 1
+echo "Waiting for container to start..."
+while [ "$(lxc-info -n apachep -s | awk '{print $2}')" != "RUNNING" ]; do
+    sleep 1
 done
 
 sleep 5  # Additional delay to ensure network readiness
